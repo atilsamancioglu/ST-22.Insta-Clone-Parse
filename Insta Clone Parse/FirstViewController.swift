bfe4ceff513ce50eb42570e34be80c0ad58c3b5c
Atil Samancioglu <samancioglu.atil@gmail.com>
2017-06-21 12:01:22 +0300

    Initial Commit

---
+++ Insta Clone Parse/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Insta Clone Parse
+//
+//  Created by Atil Samancioglu on 21/06/2017.
+//  Copyright © 2017 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
