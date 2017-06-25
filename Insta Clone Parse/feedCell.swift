ac0ffc7a16b48e30d78f92c989c8380f9b14917f
Atil Samancioglu <samancioglu.atil@gmail.com>
2017-06-25 13:28:28 +0300

    first commit

---
+++ Insta Clone Parse/feedCell.swift
@@ -0,0 +1,24 @@
+//
+//  feedCell.swift
+//  Insta Clone Parse
+//
+//  Created by Atil Samancioglu on 21/06/2017.
+//  Copyright © 2017 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class feedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# ST-22.Insta-Clone-Parse
